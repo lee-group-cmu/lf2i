2696d78036c9137a8fda3d11fc14c3446ada7c49
lucamasserano <masserano.luca@gmail.com>
2023-12-15 15:01:56 -0500

    Check for nans in pandas

--- src/lf2i/utils/miscellanea.py
+++ src/lf2i/utils/miscellanea.py
@@ -12,9 +12,12 @@ def np_to_pd(array: np.ndarray, names: Sequence[str]):
 def to_np_if_torch(inp: Union[np.ndarray, torch.Tensor]) -> np.ndarray:
     return inp if isinstance(inp, np.ndarray) else inp.cpu().detach().numpy()
 
-def check_for_nans(inp: Union[np.ndarray, torch.Tensor]) -> Union[np.ndarray, torch.Tensor]:
+def check_for_nans(inp: Union[np.ndarray, pd.Series, pd.DataFrame, torch.Tensor]) -> Union[np.ndarray, pd.Series, pd.DataFrame, torch.Tensor]:
     if isinstance(inp, np.ndarray):
         if np.isnan(inp).sum() > 0:
+            raise ValueError("Input contains NaN values")
+    elif isinstance(inp, (pd.Series, pd.DataFrame)):
+        if inp.isnull().values.any():
             raise ValueError("Input contains NaN values")
     else:
         if torch.isnan(inp).sum() > 0:
