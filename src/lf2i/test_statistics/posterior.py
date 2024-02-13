2ff1d5656c18488f2b53015f0d5b630596ab7a80
lucamasserano <masserano.luca@gmail.com>
2024-02-13 17:35:48 -0500

    Correct number of eval points in tqdm for posterior ts

--- src/lf2i/test_statistics/posterior.py
+++ src/lf2i/test_statistics/posterior.py
@@ -61,7 +61,7 @@ class Posterior(TestStatistic):
                     warnings.simplefilter('ignore', UserWarning)  # from nflows: torch.triangular_solve is deprecated in favor of ...
                     log_posterior = self.estimator.log_prob(theta=parameters, x=samples[idx, :]).double().reshape(1, parameters.shape[0])
                 return log_posterior.numpy()
-            with tqdm_joblib(tqdm(it:=range(samples.shape[0]), desc=f"Evaluating posterior for {samples.shape[0]}x{parameters.shape[0]} points ...", total=len(it))) as _:
+            with tqdm_joblib(tqdm(it:=range(samples.shape[0]), desc=f"Evaluating posterior for {samples.shape[0]} points ...", total=len(it))) as _:
                 ppr = np.vstack(Parallel(n_jobs=self.n_jobs)(delayed(eval_one)(idx) for idx in it))
             return ppr.reshape(samples.shape[0], parameters.shape[0])
         else:
@@ -126,7 +126,7 @@ class PosteriorPriorRatio(TestStatistic):
                             torch.exp(self.prior.log_prob(parameters).double()).double().reshape(parameters.shape[0], )
                     )
                 return ppr.numpy().reshape(1, parameters.shape[0])
-            with tqdm_joblib(tqdm(it:=range(samples.shape[0]), desc=f"Evaluating PPR for {samples.shape[0]}x{parameters.shape[0]} points ...", total=len(it))) as _:
+            with tqdm_joblib(tqdm(it:=range(samples.shape[0]), desc=f"Evaluating PPR for {samples.shape[0]} points ...", total=len(it))) as _:
                 ppr = np.vstack(Parallel(n_jobs=self.n_jobs)(delayed(eval_one)(idx) for idx in it))
             return ppr.reshape(samples.shape[0], parameters.shape[0])
         else:
